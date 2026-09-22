6384005a37104475397960b1f84c72fb5dce2ed8
Mohammed Shaker <am05540554@gmail.com>
2026-09-22 13:28:33 +0200

    Expose quiz and question models

--- Models/Question.cs
+++ Models/Question.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace QuizTime.Models
 {
-    internal class Question
+    // Een "Question" is een class (blauwdruk) voor één vraag in de quiz.
+    // Door dit als class te maken, kunnen we overal in de code met één
+    // "Question"-object werken in plaats van losse tekst- en lijst-variabelen.
+    public class Question
     {
+        // De vraagtekst zelf, bijvoorbeeld: "Wat is de hoofdstad van Nederland?"
+        public string Text { get; set; } = string.Empty;
+
+        // Alle antwoordmogelijkheden voor deze vraag (meestal 4 opties).
+        public List<string> Options { get; set; } = new();
+
+        // De positie (index) van het juiste antwoord in de Options-lijst.
+        // Let op: lijsten in C# beginnen te tellen vanaf 0, dus de eerste
+        // optie heeft index 0, de tweede index 1, enzovoort.
+        [JsonPropertyName("correctAnswerIndex")]
+        public int CorrectAnswerIndex { get; set; }
     }
 }
--- Models/Quiz.cs
+++ Models/Quiz.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace QuizTime.Models
 {
-    internal class Quiz
+    // De "Quiz" class bundelt een titel met alle vragen die erbij horen.
+    // Dit is het object dat we uiteindelijk uit het JSON-bestand inladen.
+    public class Quiz
     {
+        // Titel van de quiz, bijvoorbeeld "Docentenquiz Blok 3".
+        public string Title { get; set; } = string.Empty;
+
+        // De volledige lijst met vragen (elk een Question-object) van deze quiz.
+        public List<Question> Questions { get; set; } = new();
     }
 }
