cc0d8a571b13ddb310be6eb496777064b330c1e2
Mohammed Shaker <am05540554@gmail.com>
2026-09-23 09:56:27 +0200

    Remove quiz data and UI code

--- Models/Question.cs
+++ Models/Question.cs
@@ -1,14 +1,1 @@
-﻿using System.Text.Json.Serialization;
-
-public class Question
-{
-    public string Text { get; set; } = string.Empty;
-    public List<string> Options { get; set; } = new();
-
-    [JsonPropertyName("correctAnswerIndex")]
-    public int CorrectAnswerIndex { get; set; }
-
-    // الفئة اللي ينتمي لها هالسؤال، مثلاً "Dieren" أو "Geschiedenis".
-    // نستخدمها بعدين لنعرض تصنيف السؤال، أو نصفّي الأسئلة حسب الفئة.
-    public string Category { get; set; } = string.Empty;
-}
+﻿
--- Models/Quiz.cs
+++ Models/Quiz.cs
@@ -1,15 +1,1 @@
-﻿using System.Collections.Generic;
-
-namespace QuizTime.Models
-{
-    // De "Quiz" class bundelt een titel met alle vragen die erbij horen.
-    // Dit is het object dat we uiteindelijk uit het JSON-bestand inladen.
-    public class Quiz
-    {
-        // Titel van de quiz, bijvoorbeeld "Docentenquiz Blok 3".
-        public string Title { get; set; } = string.Empty;
-
-        // De volledige lijst met vragen (elk een Question-object) van deze quiz.
-        public List<Question> Questions { get; set; } = new();
-    }
-}
+﻿
